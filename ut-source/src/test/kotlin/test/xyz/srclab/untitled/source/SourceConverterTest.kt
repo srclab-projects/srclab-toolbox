2c7be5dd6ca6daa5370597a0d029410bcd7f2a63
fredsuvn <fredsuvn@163.com>
2021-04-29 11:03:20 +0800

    fix convret

--- ut-source/src/test/kotlin/test/xyz/srclab/untitled/source/SourceConverterTest.kt
+++ ut-source/src/test/kotlin/test/xyz/srclab/untitled/source/SourceConverterTest.kt
@@ -3,6 +3,7 @@ package test.xyz.srclab.untitled.source
 import org.springframework.util.AntPathMatcher
 import org.testng.annotations.Test
 import org.yaml.snakeyaml.Yaml
+import xyz.srclab.common.base.asAny
 import xyz.srclab.common.base.loadStringResource
 import xyz.srclab.untitled.source.SourceConverter
 
@@ -31,17 +32,26 @@ class SourceConverterTest {
     }
 
     private fun convert(name: String) {
+
+        fun Any?.asStringList(): List<String> {
+            return this?.asAny() ?: emptyList()
+        }
+
+        fun Any?.asStringMap(): Map<String, String> {
+            return this?.asAny() ?: emptyMap()
+        }
+
         val yamlStr = "source.yml".loadStringResource()
         val yaml = Yaml()
         val all: Map<String, Map<String, Any>> = yaml.load(yamlStr)
         val target = all[name]!!
-        val filter = target["filter"] as List<String>
-        val filterNot = target["filterNot"] as List<String>
+        val filter = target["filter"].asStringList()
+        val filterNot = target["filterNot"].asStringList()
         converter.convert(
             target["sourcePath"] as String,
             target["destPath"] as String,
-            target["replaceDir"] as Map<String, String>,
-            target["replaceContent"] as Map<String, String>,
+            target["replaceDir"].asStringMap(),
+            target["replaceContent"].asStringMap(),
         ) {
             for (s in filterNot) {
                 if (antPathMatcher.match(s, it)) {
